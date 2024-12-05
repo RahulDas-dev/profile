508862ed84a7e7f36657e56cfbc64a8ed715b8de
RahulDas-dev <r.das699@gmail.com>
2024-12-05 19:33:39 +0530

    added Blogs Section

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,20 @@ import "./styles.css";
 
 import "./navbar.ts"
 import "./manage_theme.ts"
+
+const blogs_left_button = document.getElementById('slide-left') as HTMLButtonElement;
+const blogs_right_button = document.getElementById('slide-right') as HTMLButtonElement;
+const blogs_list = document.querySelector('#blogs-list') as HTMLDivElement;
+
+blogs_left_button.addEventListener('click', () => {
+  blogs_list.scrollBy({
+    left: -300,
+    behavior: 'smooth'
+  })
+});
+blogs_right_button.addEventListener('click', () => {
+  blogs_list.scrollBy({
+    left: 300,
+    behavior: 'smooth'
+  })
+});
